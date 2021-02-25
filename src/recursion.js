742bf38b212fb180c80b5bbc42af157ae49c11ed
Charles Fowler <chas.r.fowler@gmail.com>
2021-02-24 19:59:34 -0600

    completed 1 - 10

--- src/recursion.js
+++ src/recursion.js
@@ -42,18 +42,64 @@ var arraySum = function(array) {
 };
 
 // 4. Check if a number is even.
+/*
+  var q = parseInt(n / 2);  //finding quotient (integer part only)
+  var p = q * 2;  //finding product
+  if ((n - p) === 0);  //finding modulus
+    return true;
+}
+*/
 var isEven = function(n) {
+  //n is negative
+  if (n < 0) {
+    n = Math.abs(n);
+  }
+  //n is zero
+  if (n === 0) {
+    return true;
+  }
+  //n is odd
+  if ( n === 1) {
+    return false;
+  }
+  n = n - 2
+  return isEven(n);
 };
 
 // 5. Sum all integers below a given integer.
-// sumBelow(10); // 45
+// sumBelow(10); // 45 (10 - 1) + (10 - 2)
 // sumBelow(7); // 21
+/*
+expect(sumBelow(-1)).to.equal(0);
+expect(sumBelow(-2)).to.equal(-1);
+expect(sumBelow(-6)).to.equal(-15);
+expect(sumBelow(-11)).to.equal(-55);
+*/
 var sumBelow = function(n) {
+  if (n === 0) {
+    return 0;
+  }
+  if (n < 0) {
+    n = Math.abs(n + 1)
+    return -1 * sumBelow(n + 1);
+  }
+  return (n - 1) + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x === y || x + 1 === y) {
+    return [];
+  }
+  if (x > y) {
+    var revArray = range(y, x - 1);
+    revArray.push(x - 1);
+    return revArray.reverse();
+  }
+  var array = range(x, y - 1);
+  array.push(y - 1)
+  return array;
 };
 
 // 7. Compute the exponent of a number.
@@ -62,6 +108,16 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp < 0) {
+    exp *= -1;
+    var isNeg = base * exponent(base, exp - 1);
+    return 1 / isNeg;
+  } else {
+    return base * exponent(base, exp - 1);
+  }
 };
 
 // 8. Determine if a number is a power of two.
@@ -69,14 +125,33 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 0) {
+    return false;
+  }
+  if (n === 1) {
+    return true;
+  }
+  return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0) {
+    return string;
+  }
+  return reverse(string.substring(1)) + string.charAt(0)
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  var str = string.toLowerCase().split(' ').join('');
+  if (str.length === 0 || str.length === 1) {
+    return true;
+  }
+  if (str.slice(0, 1) !== str.slice(-1)) {
+    return false;
+  }
+  return palindrome(str.slice(1, -1))
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
