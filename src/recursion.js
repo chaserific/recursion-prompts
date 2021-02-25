09fcb684ef71939fd983bec12993bd00ae677ed3
Charles Fowler <chas.r.fowler@gmail.com>
2021-02-25 11:08:03 -0600

    completed 23 and 24 of recursion prompts

--- src/recursion.js
+++ src/recursion.js
@@ -232,12 +232,43 @@ var countKeysInObj = function(obj, key) {
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
+
+// var countValuesInObj = function(obj, value) {
+//   var count = 0;
+//   for (var prop in obj) {
+//     if (obj[prop] === value) count++;
+//     if (obj[prop] instanceof Object) {
+//       count += countValuesInObj(obj[prop], value);
+//     }
+//   }
+//   return count;
+// };
 var countValuesInObj = function(obj, value) {
+  var count = 0;
+  for (var key in obj) {
+    if (obj[key] === value) {
+      count++;
+    }
+    if (obj[key] instanceof Object) {
+      count+= countValuesInObj(obj[key], value);
+    }
+  }
+  return count;
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+  for (var key in obj) {
+    if (key === oldKey) {
+      obj[newKey] = obj[key];
+      delete obj[key];
+    }
+    if (obj[key] instanceof Object) {
+      replaceKeysInObj(obj[key], oldKey, newKey);
+    }
+  }
+  return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
